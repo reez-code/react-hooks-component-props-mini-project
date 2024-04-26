338ea7f5e4120c2db9939522d1869f7bf54997ec
Phil <phil.github@gmail.com>
2024-04-26 19:02:30 +0300

    assignment completed

---
+++ src/components/ArticleList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Article from "./Article";
+const ArticleList = ({ posts }) => {
+  const articlesComponent = posts.map((post) => {
+    return (
+      <Article
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+      />
+    );
+  });
+  return <main>{articlesComponent}</main>;
+};
+
+export default ArticleList;
